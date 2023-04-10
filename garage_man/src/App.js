77c8498b929f5b8f44614e9eb3d9e80eb04e6a69
EdoToji <92823614+EdoToji@users.noreply.github.com>
2023-04-10 15:13:03 -0700

    Tickets and Vehicles pages
    Updated header to redirect to these pages

--- garage_man/src/App.js
+++ garage_man/src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
 import Login from "./pages/Login.js";
 import CreateAccount from "./pages/createAccount.js";
-//import Header from "./Header/Header.tsx";
 import PrivateRoutes from "./PrivateRoutes.js";
-import  Home from "./pages/Home.js";
+import  Home from "./pages/Home.tsx";
+import Tickets from "./pages/Tickets.tsx";
+import Vehicles from "./pages/Vehicles.tsx";
 import { Route, Routes } from "react-router-dom";
 import React from "react";
 import MainLayout from "./MainLayout.js";
@@ -16,6 +17,8 @@ function App(){
           <Route element={<PrivateRoutes/>}>
             <Route element={<MainLayout/>}>
               <Route path="/Home" element={<Home/>}/>
+              <Route path="/Tickets" element={<Tickets/>}/>
+              <Route path="/Vehicles" element={<Vehicles/>}/>
             </Route>
           </Route>
           <Route path="/" element={<Login/>}/>
