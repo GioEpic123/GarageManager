7f1cb2ed4c3df9e57d7b0773e4f0a863077b8375
Giovanni Quevedo <giovanni.quevedo5@gmail.com>
2023-04-12 12:48:45 -0700

    Fixed flow and async errors, as well as form submission

--- garage_man/src/pages/createAccount.js
+++ garage_man/src/pages/createAccount.js
@@ -3,13 +3,13 @@ import React, { useState, useEffect } from "react";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
-import { useAuthState } from "react-firebase-hooks/auth";
+
 import {
   getFirestore,
   collection,
   getDoc,
   doc,
-  addDoc,
+  setDoc,
   serverTimestamp,
 } from "firebase/firestore";
 
@@ -30,53 +30,74 @@ const firestore = getFirestore(app);
 
 // Check if the user has a vehicle by looking through the vehicles collection for
 // a vehicle with their UID associated. If none exist, prompt them to add one
+
+// CreateAccount:
+// - Check to see if a user currenty has an account on our db
+// - - If not, we prompt them to make one using their user ID as the document name
+// - - If so, we just forward them to their specific homepage
 const CreateAccount = () => {
-  // TO-DO: Fix this async part
+  // -- // Set up our variables
 
+  // Enable navigation
   const navigate = useNavigate();
-  //Check to see if our user doc exists
-
-  // Error state default to false
+  // Get reference to our collection of "users"
+  const userRef = collection(firestore, "users");
+  // Set our error state to false by default
   const [error, setError] = useState(false);
   // DocExists default to null
   const [docExists, setDocExists] = useState(null);
+  // Tell user we're loading while we get set up
   const [loadState, setLoadState] = useState("loading");
-
-  //const [userID, setUserID] = useState(null);
-
+  // Set our userID to null
+  const [userID, setUserID] = useState(null);
   // Get current user ID
-  const uid = auth.currentUser.uid;
-  // setUserID((prevState) => {
-  //   if(prevState != null)
-  //     return prevState;
-  // });
-  //setUserID(uid);
+  const currentUser = auth.currentUser;
 
-  // Get reference to collection of "users", if any
-  const userRef = collection(firestore, "users");
+  // Make a placeholder to store our document reference later
+  let userDocRef = null;
 
-  // Get a reference to where our user doc would be
-  const userDocRef = doc(userRef, uid);
+  // -- // useEffect calls - allows us to perform async operations when certain variables update (re-render)
 
-  // Effect loop:
-  // - Make call to db to get the doc
-  // - Update load state to done
+  // - useEffect gets called whenever currentUser changes
   useEffect(() => {
-    // Tell user we're loading
-    setLoadState("loading");
-    // Make async call to db
-    getDoc(userDocRef)
-      .then((docSnapshot) => {
-        // Mark that we're done, take note of doc status
-        setLoadState("done");
-        setDocExists(docSnapshot.exists());
-      })
-      .catch((err) => {
-        setLoadState("error");
-        setError(err);
-      });
-  }, []);
+    // If user isn't null, take note of it's ID and make a document reference
+    if (currentUser != null) {
+      setUserID(currentUser.uid);
 
+      // This reference gets lost every render, but that's okay for our usage. Use the below comment to supress warnings
+      // eslint-disable-next-line
+      userDocRef = doc(userRef, currentUser.uid);
+    }
+  }, [currentUser]);
+
+  // - useEffect gets called whenevr currentUser changes
+  useEffect(() => {
+    // Make async call to db if our reference is not null
+    if (userDocRef != null) {
+      getDoc(userDocRef)
+        .then((docSnapshot) => {
+          // Mark that we're done, take note of doc status
+          setLoadState("done");
+          setDocExists(docSnapshot.exists());
+        })
+        .catch((err) => {
+          // Mark that an error occured, and take note
+          setLoadState("error");
+          setError(err);
+        });
+    } else {
+      // Bad state - our user was null
+      // have user go back to root and come back to ensure user is active
+      console.log("Bad state-having user come back");
+      navigate("/");
+    }
+    // React wants us to add navigate and userDocRef to dependancies, but this causes too many renders. Use below comment to suppress warnings
+    // eslint-disable-next-line
+  }, [currentUser]);
+
+  // -- // Returns: What components we send to the user
+
+  // If we've recieved an error, let the user know
   if (loadState === "error") {
     return (
       <div>
@@ -86,9 +107,13 @@ const CreateAccount = () => {
     );
   }
 
+  // Otherwise, do the following:
+  // - Let user know that we're loading from the database
+  // - once done, if we have a document for them, send them home
+  // - otherwise, have them register and make a user document using carRegistration
   return (
     <div>
-      <p>{uid}</p>
+      <p>{userID}</p>
       {loadState === "loading" ? (
         <h1>Loading</h1>
       ) : docExists ? (
@@ -100,8 +125,11 @@ const CreateAccount = () => {
   );
 };
 
-function CarRegistrationForm(uid, userRef) {
+// Generates a form to register the user on our DB
+function CarRegistrationForm(userID) {
+  // Enable navigation and get a reference to our users collection
   const navigate = useNavigate();
+  const userRef = collection(firestore, "users");
 
   //Reference our user state to take in form data
   const [makeVal, setMakeVal] = useState("");
@@ -111,71 +139,79 @@ function CarRegistrationForm(uid, userRef) {
 
   // Nested method to publish user data on form complete
   const saveUserData = async (e) => {
-    // Get the user's data
+    // Prevent the page from refreshing when the form is submitted
     e.preventDefault();
-    await addDoc(userRef, {
+
+    // get the user's ID from our auth state
+    // note: -- we don't guard in case it's null since the user needs time to enter their car info, so at this time it should be populated
+    const uid = auth.currentUser.uid;
+    // Create a doc on the "users" collection with our ID as the name
+    await setDoc(doc(userRef, uid), {
       createdAt: serverTimestamp(),
-      uid,
+      ID: uid,
       car: { make: makeVal, model: modelVal, color: colorVal, plate: plateVal },
     });
+
+    // Once we're done adding the doc to the db, send them to the home page
+    navigate("/home");
   };
 
+  // -- Return the form to input user data
   return (
-    <form className="create-account-form" onSubmit={saveUserData}>
-      <h1 className="ca-title" style={{ fontFamily: "Poppins" }}>
-        Welcome!
-      </h1>
-      <h2>Please set up your account by putting a vehicle on file.</h2>
-      <div>
-        <label for="user-email">Make: </label>
-        <input
-          value={makeVal}
-          type="make"
-          class="car-make"
-          placeholder="Toyota"
-          onChange={(e) => setMakeVal(e.target.value)}
-        />
-      </div>
-      <div>
-        <label for="pass">Model: </label>
-        <input
-          value={modelVal}
-          type="model"
-          class="car-model"
-          placeholder="Camry"
-          onChange={(e) => setModelVal(e.target.value)}
-        />
-      </div>
-      <div>
-        <label for="pass">Color: </label>
-        <input
-          value={colorVal}
-          type="car-color"
-          class="car-color"
-          placeholder="Blue"
-          onChange={(e) => setColorVal(e.target.value)}
-        />
-      </div>
-      <div>
-        <label for="pass">License Plate: </label>
-        <input
-          value={plateVal}
-          type="text"
-          class="license-plate"
-          placeholder="1ABC234"
-          onChange={(e) => setPlateVal(e.target.value)}
-        />
-      </div>
-      <div className="btns">
-        <button
-          className="signup-btn"
-          type="submit"
-          onClick={() => navigate("/Home")}
-        >
-          Sign Up
-        </button>
-      </div>
-    </form>
+    <div>
+      <form className="create-account-form" onSubmit={saveUserData}>
+        <h1 className="ca-title" style={{ fontFamily: "Poppins" }}>
+          Welcome!
+        </h1>
+        <h2>Please set up your account by putting a vehicle on file.</h2>
+        <div>
+          <label>Make: </label>
+          <input
+            value={makeVal}
+            type="text"
+            placeholder="Toyota"
+            onChange={(e) => setMakeVal(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Model: </label>
+          <input
+            value={modelVal}
+            type="text"
+            placeholder="Camry"
+            onChange={(e) => setModelVal(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Color: </label>
+          <input
+            value={colorVal}
+            type="text"
+            placeholder="Blue"
+            onChange={(e) => setColorVal(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>License Plate: </label>
+          <input
+            value={plateVal}
+            type="text"
+            placeholder="1ABC234"
+            onChange={(e) => setPlateVal(e.target.value)}
+          />
+        </div>
+        <div className="btns">
+          {/* <button
+            className="signup-btn"
+            type="submit"
+            onClick={() => navigate("/Home")}
+          > */}
+          <button className="signup-btn" type="submit">
+            Sign Up
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
 
