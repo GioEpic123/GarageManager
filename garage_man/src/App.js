78bea3f468ca88f27839c304bb1559eebfa207c2
Giovanni Quevedo <giovanni.quevedo5@gmail.com>
2023-04-12 14:31:31 -0700

    Minor error with route tree

--- garage_man/src/App.js
+++ garage_man/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Login from "./pages/Login.js";
 import CreateAccount from "./pages/createAccount.js";
 import PrivateRoutes from "./PrivateRoutes.js";
-import  Home from "./pages/Home.tsx";
+import Home from "./pages/Home.tsx";
 import Tickets from "./pages/Tickets.tsx";
 import Vehicles from "./pages/Vehicles.tsx";
 import Settings from "./pages/Settings.tsx";
@@ -13,14 +13,13 @@ import MainLayout from "./MainLayout.js";
 function App() {
   return (
     <div className="App">
-        <Routes>
-          <Route element={<PrivateRoutes/>}>
-            <Route element={<MainLayout/>}>
-              <Route path="/Home" element={<Home/>}/>
-              <Route path="/Tickets" element={<Tickets/>}/>
-              <Route path="/Vehicles" element={<Vehicles/>}/>
-              <Route path="/Settings" element={<Settings/>}/>
-            </Route>
+      <Routes>
+        <Route element={<PrivateRoutes />}>
+          <Route element={<MainLayout />}>
+            <Route path="/Home" element={<Home />} />
+            <Route path="/Tickets" element={<Tickets />} />
+            <Route path="/Vehicles" element={<Vehicles />} />
+            <Route path="/Settings" element={<Settings />} />
           </Route>
         </Route>
         <Route path="/" element={<Login />} />
