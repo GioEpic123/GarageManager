5cd09a947cf2701b1874548f93e93f01b8790067
Giovanni Quevedo <giovanni.quevedo5@gmail.com>
2023-04-26 22:59:29 -0700

    Moved code to after our api check

--- garage_man/src/pages/Tickets.tsx
+++ garage_man/src/pages/Tickets.tsx
@@ -15,18 +15,17 @@ const firestore = getFirestore(app);
 //constructor for ticket data type, probably not needed anymore
 export class TicketInfo {
   constructor(
-    public date: Date = new Date,
-    public checkIn: Date = new Date,
-    public checkOut: Date = new Date,
+    public date: Date = new Date(),
+    public checkIn: Date = new Date(),
+    public checkOut: Date = new Date(),
     public active: boolean,
     public price: Number
-  ) {  }
-
+  ) {}
 }
 //create ticket type that has date(Date), startTime(time), duration(int), active(bool), and price(double)
 
 //array that holds the tickets, probably not needed anymore
-const ticketData: TicketInfo[]=[];
+const ticketData: TicketInfo[] = [];
 
 const Tickets = () => {
   const [loadState, setLoadState] = useState("loading");
@@ -72,74 +71,63 @@ const Tickets = () => {
   return (
     <div className="Tickets">
       <view>
-        <text>
-          <h1>Tickets</h1>
-          <h1>Active Tickets</h1>
-          <table className="activeTable">
-          <tr>
-            <th>Date</th>
-            <th>Check-In</th>
-            <th>Check-Out</th>
-            <th>Payment</th>
-          </tr>
-          {snapshot.docs.map((val, key) => {
-          if(val.data().active == true){
-          //let dateMDY = `${val.date.getMonth() + 1}/${val.date.getDate()}/${val.date.getFullYear()}`;
-          //let checkInTime = `${val.checkIn.getHours()}:${String(val.checkIn.getMinutes()).padStart(2, '0')}`;
-          //let checkOutTime = `${val.checkOut.getHours()}:${String(val.checkOut.getMinutes()).padStart(2, '0')}`;
-          return (
-            <tr key={key = val.ID}>
-              <td>{String(val.data().createdAt)}</td>
-              <td>{String(val.data().startTime)}</td>
-              <td>{String(val.data().endTime)}</td>
-              <td>{String(val.data().price)}</td>
-            </tr>
-            )
-          }})}
-          </table>
-        <h1>Inactive Tickets</h1>
-          <table className="inactiveTable">
-          <tr>
-            <th>Date</th>
-            <th>Check-In</th>
-            <th>Check-Out</th>
-            <th>Payment</th>
-          </tr>
-          {snapshot.docs.map((val, key) => {
-          if(val.data().active == false){
-          //let dateMDY = `${val.date.getMonth() + 1}/${val.date.getDate()}/${val.date.getFullYear()}`;
-          //let checkInTime = `${val.checkIn.getHours()}:${String(val.checkIn.getMinutes()).padStart(2, '0')}`;
-          //let checkOutTime = `${val.checkOut.getHours()}:${String(val.checkOut.getMinutes()).padStart(2, '0')}`;
-          return (
-            <tr key={key = val.ID}>
-              <td>{String(val.data().createdAt)}</td>
-              <td>{String(val.data().startTime)}</td>
-              <td>{String(val.data().endtime)}</td>
-              <td>{String(val.data().price)}</td>
-            </tr>
-            )
-          }})}
-          </table>
-        </text>
         <section>
           {loadState === "loading" ? (
             <h2>Loading...</h2>
           ) : snapshot.docs.length === 0 ? (
             <p>No tickets to display. Make some!</p>
           ) : (
-            <section>
-              {snapshot.docs.map((doc) => {
-                return (
-                  <div key={doc.ID}>
-                    <h2>
-                      {doc.data().startTime} - Active: {doc.data().active}
-                    </h2>
-                    <p>{doc.data().duration} hrs at $10/hr</p>
-                    <p>Subtotal: ${doc.data().price}</p>
-                  </div>
-                );
-              })}
-            </section>
+            <text>
+              <h1>Tickets</h1>
+              <h1>Active Tickets</h1>
+              <table className="activeTable">
+                <tr>
+                  <th>Date</th>
+                  <th>Check-In</th>
+                  <th>Check-Out</th>
+                  <th>Payment</th>
+                </tr>
+                {snapshot.docs.map((val, key) => {
+                  //if (val.data().active == true) {
+                  //let dateMDY = `${val.date.getMonth() + 1}/${val.date.getDate()}/${val.date.getFullYear()}`;
+                  //let checkInTime = `${val.checkIn.getHours()}:${String(val.checkIn.getMinutes()).padStart(2, '0')}`;
+                  //let checkOutTime = `${val.checkOut.getHours()}:${String(val.checkOut.getMinutes()).padStart(2, '0')}`;
+                  return (
+                    <tr key={(key = val.ID)}>
+                      <td>{String(val.data().createdAt)}</td>
+                      <td>{String(val.data().startTime)}</td>
+                      <td>{String(val.data().endTime)}</td>
+                      <td>{String(val.data().price)}</td>
+                    </tr>
+                  );
+                  //}
+                })}
+              </table>
+              <h1>Inactive Tickets</h1>
+              <table className="inactiveTable">
+                <tr>
+                  <th>Date</th>
+                  <th>Check-In</th>
+                  <th>Check-Out</th>
+                  <th>Payment</th>
+                </tr>
+                {snapshot.docs.map((val, key) => {
+                  //if (val.data().active == false) {
+                  //let dateMDY = `${val.date.getMonth() + 1}/${val.date.getDate()}/${val.date.getFullYear()}`;
+                  //let checkInTime = `${val.checkIn.getHours()}:${String(val.checkIn.getMinutes()).padStart(2, '0')}`;
+                  //let checkOutTime = `${val.checkOut.getHours()}:${String(val.checkOut.getMinutes()).padStart(2, '0')}`;
+                  return (
+                    <tr key={(key = val.ID)}>
+                      <td>{String(val.data().createdAt)}</td>
+                      <td>{String(val.data().startTime)}</td>
+                      <td>{String(val.data().endtime)}</td>
+                      <td>{String(val.data().price)}</td>
+                    </tr>
+                  );
+                  //}
+                })}
+              </table>
+            </text>
           )}
         </section>
       </view>
